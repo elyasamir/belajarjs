990b4ecd79d82c653629278aedbec5df9b0761d2
ElyasAmir <elyas.amir192@gmail.com>
2019-05-13 16:21:01 +0800

    first commit

---
+++ myscript.js
@@ -0,0 +1,61 @@
+// alert("welcome to your bank!")
+//
+// var deposit = prompt("How much would you like to deposite today")
+// alert("you deposited :" + deposit)
+// console.log("elyas a cool person")
+
+// var lbs = prompt("weight in lbs? : ")
+// var kg = lbs * 0.454
+// alert("that is "+kg+" kilograms!")
+// console.log("conversion complete")
+
+// var hot = false
+// var temp = 40
+//
+// if (temp>80) {
+//   console.log("its hot today")
+// }else if(temp<=80 && temp>=50) {
+//   console.log("average temperature")
+// }else {
+//   console.log("its very cold")
+// }
+
+
+// var x = 0
+//
+// while (x<5) {
+//   console.log("x is "+ x);
+//
+//   if (x==3) {
+//     console.log("X EQUAL TO 3!!!!");
+//     break;
+//   }
+//   console.log("x is still below 5");
+//   x = x + 1;
+// }
+
+// var num =1
+//
+// while (num<11) {
+//   if (num%2===0) {
+//     console.log(num);
+//   }
+//   num = num + 1;
+//
+// }
+
+// for (var i = 0; i < 5; i++) {
+//   console.log("number is "+i)
+// }
+
+function  hello(){
+  console.log("hello world");
+}
+
+function  helloyou(name){
+  console.log("hello world "+name);
+}
+
+function addNum(num1,num2){
+  console.log(num1+num2)
+}
